3ceae9d7ac441be84d306c6c3e6ce2ba901da2f4
vitor melo <vitormelofreitas01@gmail.com>
2024-07-28 15:48:20 -0300

    teste 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import logo from "../assets/images/"
+import logo from "../assets/images/logo"
 
 
 export const Header = () => {
